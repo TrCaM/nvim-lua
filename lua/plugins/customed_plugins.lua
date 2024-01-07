d2e5c87e26fe525d0a22a5bc196b0ae8cb94de37
Tri Cao <cathy.dcpt@gmail.com>
2024-01-07 16:57:17 -0500

    Fix bug in customed plugins

--- lua/plugins/customed_plugins.lua
+++ lua/plugins/customed_plugins.lua
@@ -1,11 +1,14 @@
 return {
-  "tris203/hawtkeys.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = {
-    -- an empty table will work for default config
-    --- if you use functions, or whichky, or lazy to map keys
-    --- then please see the API below for options
+  {
+    "tris203/hawtkeys.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = {
+      -- an empty table will work for default config
+      --- if you use functions, or whichky, or lazy to map keys
+      --- then please see the API below for options
+    },
   },
+
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
