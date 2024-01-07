68609c48559493b1f02471ec832adfb52126c778
Tri Cao <cathy.dcpt@gmail.com>
2024-01-07 16:25:42 -0500

    Lazy vim configuration 2024

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "sainnhe/gruvbox-material" },
+  { "catppuccin/nvim" },
+  { "rose-pine/neovim" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/sonokai" },
+  { "folke/tokyonight.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
