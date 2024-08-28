727c60f14191328710950e0404822bc0feea1e34
caot <caot@google.com>
2024-08-28 17:40:17 +0000

    Beautify markdown rendering and workflow

--- lua/plugins/customed_plugins.lua
+++ lua/plugins/customed_plugins.lua
@@ -64,4 +64,17 @@ return {
 
   -- disable trouble
   { "nvim-notify", enabled = false },
+
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+        { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 }
